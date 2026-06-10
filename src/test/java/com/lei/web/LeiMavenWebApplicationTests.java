7993927339db5d28a1b72c1a186b5c513af822f9
leizh6060 <leizh6060@yahoo.com>
2026-06-09 22:14:44 -0400

    Initial commit: Java Web project with GH Actions

---
+++ src/test/java/com/lei/web/LeiMavenWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lei.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeiMavenWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
